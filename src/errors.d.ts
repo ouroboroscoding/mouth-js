b497913ed6fbd7c3578850af6c8b1842ff67c8bd
Chris Nasr <chris@ouroboroscoding.com>
2023-03-08 11:07:10 -0500

    Release - Updated version - Added baseURL to tsconfig.json - Added errors module - Added default module export, an instance of body.Service using 'mouth' - Moved @ouroboros/body from a dependency to a peer dependency - Removed @ouroboros/events and @ouroboros/subscribe - Removed nouns module (replaced with mouth) - Removed locales module (now in mouth-mui)

---
+++ src/errors.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Errors
+ *
+ * Contains constants to describe errors from the mouth service
+ *
+ * @author Chris Nasr <chris@ouroboroscoding.com>
+ * @copyright Ouroboros Coding Inc.
+ * @created 2023-03-07
+ */
+export { errors as body } from '@ouroboros/body';
+export declare const TEMPLATE_CONTENT_ERROR = 1300;
+export declare const ATTACHMENT_STRUCTURE = 1301;
+export declare const ATTACHMENT_DECODE = 1302;
+export declare const SMTP_ERROR = 1303;
---
+++ src/index.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Mouth
+ *
+ * Main export of @ouroboros/mouth module
+ *
+ * @author Chris Nasr <chris@ouroboroscoding.com>
+ * @copyright Ouroboros Coding Inc.
+ * @created 2023-03-07
+ */
+import { Service } from '@ouroboros/body';
+export * as errors from './errors';
+declare const mouth: Service;
+export default mouth;
